9a54ccb1f140e70885c1f1e4b2ded87baf1758ec
Senthil <senthil.kalimuthu@tarento.com>
2018-05-27 01:55:53 +0530

    introducing argparser

--- dag_paths.py
+++ dag_paths.py
@@ -1,6 +1,12 @@
 import networkx as nx
+import argparse
 
 G=nx.DiGraph()
+parser = argparse.ArgumentParser()
+
+def parse_args():
+    parser.add_argument("-f", "--file", help="path to data file")
+    return parser.parse_args()
 
 def add_edge(v1, v2):
     G.add_edge(v1, v2)
@@ -8,10 +14,9 @@ def add_edge(v1, v2):
 def add_node(n):
     G.add_node(n)
 
-if __name__ == '__main__':
-    data_file = input()
-
-    with open(data_file) as f:
+def main():
+    args = parse_args()
+    with open(args.file) as f:
         data = f.readlines()
 
     for i in range(int(data[0])):
@@ -34,3 +39,6 @@ if __name__ == '__main__':
             for path in nx.all_simple_paths(G, source=source, target=dest):
                 print('->'.join(str(i) for i in path))
     print('\n'.join(str(i) for i in list(nx.isolates(G))))
+
+if __name__ == '__main__':
+    main()
